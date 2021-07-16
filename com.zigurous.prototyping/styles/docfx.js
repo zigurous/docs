efb95845a86baf74da612fde2c50223c55f751ee
adamgraham <adamgraham@users.noreply.github.com>
2021-07-16 07:14:50 +0000

    deploy: zigurous/unity-prototyping-kit@cf18b4d35e96ae8ef2e78499653db7e4889e2606

--- com.zigurous.prototyping/styles/docfx.js
+++ com.zigurous.prototyping/styles/docfx.js
@@ -54,7 +54,7 @@ $(function () {
   // Styling for tables in conceptual documents using Bootstrap.
   // See http://getbootstrap.com/css/#tables
   function renderTables() {
-    $("table").addClass("table table-bordered table-striped table-condensed");
+    $("table").addClass("table table-bordered table-condensed");
   }
 
   // Styling for alerts.
